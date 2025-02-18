65799819c7bbadc20c86cde281a4bac344058f74
Bruno Henrique Oliveira <bh084996074995@gmail.com>
2025-02-18 16:29:33 -0300

    Criando a função para atualizar a lista

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,47 @@ import TodoForm from './components/TodoForm';
 function App() {
   const [todos, setTodos] = useState([
     {
-      id:1,
-      text:"Criar funcionalidade X no sistema", 
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
       category: "Trabalho",
       isCompleted: false,
     },
     {
-      id:2,
-      text:"Estudar React", 
+      id: 2,
+      text: "Estudar React",
       category: "Estudos",
       isCompleted: false,
     },
     {
-      id:3,
-      text:"Ir para a academia", 
+      id: 3,
+      text: "Ir para a academia",
       category: "Pessoal",
       isCompleted: false,
     },
   ])
 
+  const addTodo = (text, category) => {
+    const newTodos =
+      [...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+      ];
+      setTodos(newTodos);
+  };
+
   return (
     <div className="app">
-       <h1> Lista de Tarefas </h1>
-       <div className="todo-list">
-        {todos.map((todo) => ( 
+      <h1> Lista de Tarefas </h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
           <Todo key={todo.id} todo={todo} />
         ))}
-       </div>
-       <TodoForm />
+      </div>
+      <TodoForm addTodo={addTodo}/>
     </div>
   )
 }
