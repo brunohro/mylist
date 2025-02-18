50480ff042bbbacf0bb5ac6ecfa7c344b7c8fab4
Bruno Henrique Oliveira <bh084996074995@gmail.com>
2025-02-18 10:25:48 -0300

    Criando o primeiro componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css';
+import Todo from './components/Todo';
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -28,16 +30,7 @@ function App() {
        <h1> Lista de Tarefas </h1>
        <div className="todo-list">
         {todos.map((todo) => ( 
-          <div className="todo">
-            <div className="content">
-              <p>{todo.text}</p>
-              <p className="category">({todo.category})</p>
-            </div>
-            <div>
-              <button> Completar </button>
-              <button> X </button>
-            </div>
-          </div>
+          <Todo todo={todo} />
         ))}
        </div>
     </div>
